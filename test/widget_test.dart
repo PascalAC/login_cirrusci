f7c130d5e629b0160624868dbda780ff5c426ec7
Pascal Brosinski <pascal@brosinski.eu>
2019-02-16 13:58:39 +0100

    Add tests with one failing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,67 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:login_cirrusci/src/app.dart';
 
 void main() {
-  
+
+  final emailKeyString = 'emailInput';
+  final passwordKeyString = 'passwordInput';
+
+  final emailErrorText = 'Enter a valid email.';
+  final passwordErrorText = 'Password must be at least 4 characters.';
+
+  testWidgets('Login button available', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+
+    expect(find.text('LOGIN'), findsOneWidget);
+  });
+
+  testWidgets('Text input fields available', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+
+    expect(find.byType(TextField), findsNWidgets(2));
+  });
+
+  testWidgets('Test non valid input', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+
+    final emailWidget = find.byKey(Key(emailKeyString));
+    final passwordWidget = find.byKey(Key(passwordKeyString));
+
+    expect(emailWidget, findsOneWidget);
+
+    await tester.enterText(emailWidget, 'abc');
+    await tester.pump();
+    
+    expect(find.text(emailErrorText), findsOneWidget);
+
+    expect(passwordWidget, findsOneWidget);
+
+    await tester.enterText(passwordWidget, '123');
+    await tester.pump();
+
+    expect(find.text(passwordErrorText), findsOneWidget);
+  });
+
+  testWidgets('Test valid input', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+
+    final emailWidget = find.byKey(Key(emailKeyString));
+    final passwordWidget = find.byKey(Key(passwordKeyString));
+
+    expect(emailWidget, findsOneWidget);
+
+    await tester.enterText(emailWidget, 'you@example.com');
+    await tester.pump();
+
+    expect(find.text(emailErrorText), findsNothing);
+
+    expect(passwordWidget, findsOneWidget);
+
+    await tester.enterText(emailWidget, '12345678');
+    await tester.pump();
+
+    expect(find.text(passwordErrorText), findsOneWidget);
+  });
 }
